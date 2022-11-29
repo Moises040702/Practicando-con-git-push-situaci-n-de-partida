85a20225778f2512034412f826b98fca4fd409e9
Moisés <moises.celmar@educa.jcyl.es>
2022-11-29 12:29:14 +0100

    Practicando con git push: pasos finales

--- App.java
+++ App.java
@@ -1,9 +1,12 @@
 public class App {
   
   public static void main(String[] args) {
+    System.out.println("Moisés");
     System.out.println("Contenido 2a");
     System.out.println("Contenido 3");
     System.out.println("Contenido 2b");
+    System.out.println("29/11/2022");
+    System.out.println("Trobajo del Camino");
   }
   
 }
